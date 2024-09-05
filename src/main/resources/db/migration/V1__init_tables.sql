3f8ef98e739071aadf515e4b76d7336294c54bd0
Alex Salavei <aliakseisalavei.contact@gmail.com>
2024-09-05 16:55:23 +0400

    feat: add SQL script `V1__init_tables.sql`

---
+++ src/main/resources/db/migration/V1__init_tables.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS players (
+    id   SERIAL  PRIMARY KEY,
+    name VARCHAR NOT NULL UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS matches (
+    id               SERIAL PRIMARY KEY,
+    first_player_id  INT    NOT NULL,
+    second_player_id INT    NOT NULL,
+    winner_id        INT    NOT NULL,
+    FOREIGN KEY (first_player_id)  REFERENCES players(id),
+    FOREIGN KEY (second_player_id) REFERENCES players(id),
+    FOREIGN KEY (winner_id)        REFERENCES players(id),
+    CONSTRAINT check_players_unique CHECK (first_player_id <> second_player_id)
+);
