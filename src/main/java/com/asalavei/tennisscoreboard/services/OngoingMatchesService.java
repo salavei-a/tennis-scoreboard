9eb9062689eff28baebf58c05cf472883a5f837f
Alex Salavei <aliakseisalavei.contact@gmail.com>
2024-09-11 20:00:13 +0400

    feat: add `OngoingMatchesService`

---
+++ src/main/java/com/asalavei/tennisscoreboard/services/OngoingMatchesService.java
@@ -0,0 +1,54 @@
+package com.asalavei.tennisscoreboard.services;
+
+import com.asalavei.tennisscoreboard.dbaccess.entities.PlayerEntity;
+import com.asalavei.tennisscoreboard.dbaccess.mapper.EntityMapper;
+import com.asalavei.tennisscoreboard.dbaccess.mapper.PlayerEntityMapper;
+import com.asalavei.tennisscoreboard.dbaccess.repositories.HibernatePlayerRepository;
+import com.asalavei.tennisscoreboard.dbaccess.repositories.PlayerRepository;
+import com.asalavei.tennisscoreboard.dto.Match;
+import com.asalavei.tennisscoreboard.dto.Player;
+import org.mapstruct.factory.Mappers;
+
+import java.util.Optional;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class OngoingMatchesService {
+
+    private static final ConcurrentHashMap<UUID, Match> ongoingMatches = new ConcurrentHashMap<>();
+
+    private final PlayerRepository playerRepository = new HibernatePlayerRepository();
+
+    private final EntityMapper<Player, PlayerEntity> mapper = Mappers.getMapper(PlayerEntityMapper.class);
+
+    public UUID create(Player firstPlayerToPlay, Player secondPlayerToPlay) {
+        Optional<PlayerEntity> firstPlayerFound = playerRepository.findByName(firstPlayerToPlay.getName());
+        Optional<PlayerEntity> secondPlayerFound = playerRepository.findByName(secondPlayerToPlay.getName());
+
+        PlayerEntity firstPlayerEntity = firstPlayerFound.orElseGet(() -> playerRepository.save(mapper.toEntity(firstPlayerToPlay)));
+        PlayerEntity secondPlayerEntity = secondPlayerFound.orElseGet(() -> playerRepository.save(mapper.toEntity(secondPlayerToPlay)));
+
+        Player firstPlayer = mapper.toDto(firstPlayerEntity);
+        Player secondPlayer = mapper.toDto(secondPlayerEntity);
+
+        firstPlayer.setGamePoints("0");
+        secondPlayer.setGamePoints("0");
+
+        UUID uuid = UUID.randomUUID();
+
+        ongoingMatches.put(uuid, Match.builder()
+                .firstPlayer(firstPlayer)
+                .secondPlayer(secondPlayer)
+                .build());
+
+        return uuid;
+    }
+
+    public Match getOngoingMatch(UUID uuid) {
+        return ongoingMatches.get(uuid);
+    }
+
+    public void removeMatch(UUID uuid) {
+        ongoingMatches.remove(uuid);
+    }
+}
