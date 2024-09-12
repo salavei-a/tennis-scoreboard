51d34e38d06b501b0e28aaeed64187a76f13f7d1
Alex Salavei <aliakseisalavei.contact@gmail.com>
2024-09-12 13:48:53 +0400

    feat: add `findAll()` and `findAllByPlayerName()` to `MatchRepository ` and implement in `HibernateMatchRepository`

--- src/main/java/com/asalavei/tennisscoreboard/dbaccess/repositories/HibernateMatchRepository.java
+++ src/main/java/com/asalavei/tennisscoreboard/dbaccess/repositories/HibernateMatchRepository.java
@@ -2,9 +2,12 @@ package com.asalavei.tennisscoreboard.dbaccess.repositories;
 
 import com.asalavei.tennisscoreboard.dbaccess.HibernateUtil;
 import com.asalavei.tennisscoreboard.dbaccess.entities.MatchEntity;
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+
+import java.util.List;
 
 public class HibernateMatchRepository implements MatchRepository {
 
@@ -20,13 +23,64 @@ public class HibernateMatchRepository implements MatchRepository {
             session.persist(match);
 
             transaction.commit();
+
+            return match;
         } catch (Exception e) {
             if (transaction != null && transaction.isActive()) {
                 transaction.rollback();
             }
-            e.printStackTrace(); // TODO: handle custom exception
-        }
 
-        return match;
+            throw new RuntimeException(e); // TODO: handle custom exception
+        }
+    }
+
+    @Override
+    public List<MatchEntity> findAll() {
+        Transaction transaction = null;
+
+        try (Session session = sessionFactory.getCurrentSession()) {
+            transaction = session.beginTransaction();
+
+            List<MatchEntity> matches = session.createQuery(
+                            "from MatchEntity m join fetch m.firstPlayer join fetch m.secondPlayer", MatchEntity.class
+                    )
+                    .list();
+
+            transaction.commit();
+
+            return matches;
+        } catch (HibernateException e) {
+            if (transaction != null && transaction.isActive()) {
+                transaction.rollback();
+            }
+
+            throw new RuntimeException(e); // TODO: handle custom exception
+        }
+    }
+
+    @Override
+    public List<MatchEntity> findAllByPlayerName(String name) {
+        Transaction transaction = null;
+
+        try (Session session = sessionFactory.getCurrentSession()) {
+            transaction = session.beginTransaction();
+
+            List<MatchEntity> matches = session.createQuery(
+                            "from MatchEntity m join fetch m.firstPlayer join fetch m.secondPlayer " +
+                               "where m.firstPlayer.name = :name or m.secondPlayer.name = :name", MatchEntity.class
+                    )
+                    .setParameter("name", name)
+                    .list();
+
+            transaction.commit();
+
+            return matches;
+        } catch (HibernateException e) {
+            if (transaction != null && transaction.isActive()) {
+                transaction.rollback();
+            }
+
+            throw new RuntimeException(e); // TODO: handle custom exception
+        }
     }
 }
--- src/main/java/com/asalavei/tennisscoreboard/dbaccess/repositories/MatchRepository.java
+++ src/main/java/com/asalavei/tennisscoreboard/dbaccess/repositories/MatchRepository.java
@@ -2,5 +2,10 @@ package com.asalavei.tennisscoreboard.dbaccess.repositories;
 
 import com.asalavei.tennisscoreboard.dbaccess.entities.MatchEntity;
 
+import java.util.List;
+
 public interface MatchRepository extends CrudRepository<MatchEntity> {
+    List<MatchEntity> findAll();
+
+    List<MatchEntity> findAllByPlayerName(String name);
 }
