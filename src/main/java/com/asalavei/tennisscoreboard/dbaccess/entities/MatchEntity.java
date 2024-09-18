1195c7121aa9b568487697b4249127c0729c533a
Alex Salavei <aliakseisalavei.contact@gmail.com>
2024-09-18 18:08:19 +0400

    refactor: align entities with database schema

--- src/main/java/com/asalavei/tennisscoreboard/dbaccess/entities/MatchEntity.java
+++ src/main/java/com/asalavei/tennisscoreboard/dbaccess/entities/MatchEntity.java
@@ -1,31 +1,38 @@
 package com.asalavei.tennisscoreboard.dbaccess.entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
+import org.hibernate.annotations.Check;
 
-@Entity
-@Table(name = "matches")
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
-@ToString
+@ToString(onlyExplicitlyIncluded = true)
 @Builder
+@Check(constraints = "first_player_id <> second_player_id")
+@Entity
+@Table(name = "matches")
 public class MatchEntity {
-
     @Id
+    @ToString.Include
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Integer id;
 
-    @ManyToOne
-    @JoinColumn(name = "first_player_id")
+    @NotNull
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "first_player_id", nullable = false)
     private PlayerEntity firstPlayer;
 
-    @ManyToOne
-    @JoinColumn(name = "second_player_id")
+    @NotNull
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "second_player_id", nullable = false)
     private PlayerEntity secondPlayer;
 
-    @ManyToOne
-    @JoinColumn(name = "winner_id")
+    @NotNull
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "winner_id", nullable = false)
     private PlayerEntity winner;
 }
--- src/main/java/com/asalavei/tennisscoreboard/dbaccess/entities/PlayerEntity.java
+++ src/main/java/com/asalavei/tennisscoreboard/dbaccess/entities/PlayerEntity.java
@@ -1,22 +1,24 @@
 package com.asalavei.tennisscoreboard.dbaccess.entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
-@Entity
-@Table(name = "players")
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
 @ToString
 @Builder
+@Entity
+@Table(name = "players")
 public class PlayerEntity {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Integer id;
 
-    @Column(name = "name")
+    @NotNull
+    @Column(name = "name", length = 50, nullable = false, unique = true)
     private String name;
 }
